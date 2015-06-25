210d4bf5f8abe556ff7c2f897b209604d9984939
moylop260 <moylop260@vauxoo.com>
2015-06-25 21:33:14 +0000

    [REF] webhook: Add all methods

--- webhook_github/models/webhook.py
+++ webhook_github/models/webhook.py
@@ -36,13 +36,8 @@ class Webhook(models.Model):
                     'X-Github-Event')
 
     @api.one
-    def run_webhook_github_status(self):
-        sys.stdout.write("I'm here: run_webhook_github_status\n")
-        # pprint(self.env.request.jsonrequest)
-        return True
-
-    @api.one
     def run_webhook_github_pull_request(self):
+        """
         sys.stdout.write("I'm here: run_webhook_github_pull_request\n")
         # pprint(self.env.request.jsonrequest)
         sys.stdout.write("PR change in: %s/%s/pull/%s\n" % (
@@ -50,10 +45,12 @@ class Webhook(models.Model):
             self.env.request.jsonrequest['repository']['name'],
             self.env.request.jsonrequest['pull_request']['number']
         ))
+        """
         return True
 
     @api.one
     def run_webhook_github_push(self):
+        """
         sys.stdout.write("I'm here: run_webhook_github_push\n")
         pprint(self.env.request.jsonrequest)
         sys.stdout.write("Push change in: %s/%s\n" % (
@@ -68,4 +65,81 @@ class Webhook(models.Model):
             "self.env.request.jsonrequest['base_ref']")
         sys.stdout.write(
             self.env.request.jsonrequest['base_ref'] + "\n")
+        """
+        return True
+
+    @api.one
+    def run_webhook_github_commit_comment(self):
+        return True
+
+    @api.one
+    def run_webhook_github_create(self):
+        return True
+
+    @api.one
+    def run_webhook_github_delete(self):
+        return True
+
+    @api.one
+    def run_webhook_github_deployment(self):
+        return True
+
+    @api.one
+    def run_webhook_github_deployment_status(self):
+        return True
+
+    @api.one
+    def run_webhook_github_fork(self):
+        return True
+
+    @api.one
+    def run_webhook_github_gollum(self):
+        return True
+
+    @api.one
+    def run_webhook_github_issue_comment(self):
+        return True
+
+    @api.one
+    def run_webhook_github_issues(self):
+        return True
+
+    @api.one
+    def run_webhook_github_member(self):
+        return True
+
+    @api.one
+    def run_webhook_github_membership(self):
+        return True
+
+    @api.one
+    def run_webhook_github_page_build(self):
+        return True
+
+    @api.one
+    def run_webhook_github_public(self):
+        return True
+
+    @api.one
+    def run_webhook_github_pull_request_review_comment(self):
+        return True
+
+    @api.one
+    def run_webhook_github_repository(self):
+        return True
+
+    @api.one
+    def run_webhook_github_release(self):
+        return True
+
+    @api.one
+    def run_webhook_github_status(self):
+        return True
+
+    @api.one
+    def run_webhook_github_team_add(self):
+        return True
+
+    @api.one
+    def run_webhook_github_watch(self):
         return True
