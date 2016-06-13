049b3ae030c185f402c2b4cfafb5bed4b93ac336
Nhomar Hernández [Vauxoo] <nhomar@vauxoo.com>
2016-06-13 12:09:06 -0500

    8.0 meshrelation (#54)
    * [IMP] module_dependencies_view: The ID is conditional.
    
    In order to allow this method be reused in other
    visualization methods.
    
    * Other options of diagrams.
    
    * [IMP] module_dependencies_view: Smart color setted to graph.
    
    One Level view working

--- module_dependencies_view/controllers.py
+++ module_dependencies_view/controllers.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-from datetime import timedelta
 from openerp import http
-from pprint import pprint
+
 
 class ModuleDependenciesView(http.Controller):
+
+    @http.route('/module/one/<model("ir.module.module"):module>/',
+                auth='user')
+    def module_level(self, module):
+        start = datetime.now()
+        depends = module.with_context({'one_level': True}).depends()
+        end = datetime.now()
+        computed = end - start
+        return http.request.render('module_dependencies_view.module', {
+            'object': module,
+            'tree': depends,
+            'start': start,
+            'end': end,
+            'computed': computed,
+        })
 
     @http.route('/module/<model("ir.module.module"):module>/',
                 auth='user')
@@ -20,3 +34,25 @@ class ModuleDependenciesView(http.Controller):
             'end': end,
             'computed': computed,
         })
+
+    @http.route('/module/dendrogram/<model("ir.module.module"):module>/',
+                auth='user')
+    def cluster_dendgram(self, module):
+        depends = module.with_context({'with_id': True}).depends()
+        return depends
+
+    @http.route('/module/mesh/<model("ir.module.module"):module>/',
+                auth='user')
+    def module_mesh(self, module):
+        start = datetime.now()
+        depends = module.depends()
+        end = datetime.now()
+        computed = end - start
+        return http.request.render(
+            'module_dependencies_view.cluster_dendrogram', {
+                'object': module,
+                'tree': depends,
+                'start': start,
+                'end': end,
+                'computed': computed,
+            })
