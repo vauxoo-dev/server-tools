82c8933da3554b39d27d7ad99caaf1a8c7579d50
Nhomar Hernández <nhomar@vauxoo.com>
2016-06-13 08:34:24 -0500

    [IMP] module_dependencies_view: Smart color setted to graph.
    One Level view working

--- module_dependencies_view/controllers.py
+++ module_dependencies_view/controllers.py
@@ -2,7 +2,23 @@
 from datetime import datetime
 from openerp import http
 
+
 class ModuleDependenciesView(http.Controller):
+
+    @http.route('/module/one/<model("ir.module.module"):module>/',
+                auth='user')
+    def module_level(self, module):
+        start = datetime.now()
+        depends = module.with_context({'one_level': True}).depends()
+        end = datetime.now()
+        computed = end - start
+        return http.request.render('module_dependencies_view.module', {
+            'object': module,
+            'tree': depends,
+            'start': start,
+            'end': end,
+            'computed': computed,
+        })
 
     @http.route('/module/<model("ir.module.module"):module>/',
                 auth='user')
@@ -23,7 +39,6 @@ class ModuleDependenciesView(http.Controller):
                 auth='user')
     def cluster_dendgram(self, module):
         depends = module.with_context({'with_id': True}).depends()
-        print depends
         return depends
 
     @http.route('/module/mesh/<model("ir.module.module"):module>/',
