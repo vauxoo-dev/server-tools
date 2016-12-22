24301384a9cee0ebf8366495dcb006a60482263f
Jose Suniaga [Vauxoo] <josemiguel@vauxoo.com>
2016-12-22 06:23:06 +0000

    [ADD] company_country: module to install country from .travis.yml
    account module use the hook _auto_install_l10n to auto-install
    l10n_generic_coa module based on country of SUPERUSER_ID
    
    using company_country module you can enable a country in
    enviroment variable to set a country for SUPERUSER_ID before
    account module installation.

---
+++ company_country/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2016  Vauxoo (<http://www.vauxoo.com/>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Country Company",
+    "summary": "Set country to main company",
+    "version": "10.0.1.0.0",
+    "category": "base",
+    "website": "https://odoo-community.org/",
+    "author": "Vauxoo, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [],
+    "data": ['data/res_config.xml'],
+    "sequence": 1,
+    "auto_install": False,
+    "installable": True,
+}
