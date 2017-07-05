778c315f8b589ba72dfe1689ca58ea90a8f1ccc8
Moisés López <moylop260@vauxoo.com>
2017-07-05 18:10:28 +0000

    fixup! [REF] profiler: Compatibility with odoo 9.0

--- profiler/tests/test_profiler.py
+++ profiler/tests/test_profiler.py
@@ -2,16 +2,15 @@
 # License AGPL-3 or later (http://www.gnu.org/licenses/lgpl).
 # Copyright 2016 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
 
-import unittest
-
-from openerp import tests
+from odoo import tests
 
 
 @tests.at_install(False)
 @tests.post_install(True)
 class TestProfiler(tests.HttpCase):
 
-    @unittest.skip("phantomjs tests async for 8.0 are so flaky")
     def test_profiler_tour(self):
-        self.phantom_js('/web', "openerp.Tour.run('profile_run', 'test')",
-                        'openerp.Tour.tours.profile_run', login='admin')
+        self.phantom_js('/',
+                        "odoo.__DEBUG__.services['web.Tour'].run('profile')",
+                        "odoo.__DEBUG__.services['web.Tour'].tours.profile",
+                        login='admin')
